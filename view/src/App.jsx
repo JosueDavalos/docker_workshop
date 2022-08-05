e12d5b1d91298c17ec6aadfff60a6c0beb263168
JosueDavalos <jedavalo@espol.edu.ec>
2022-08-05 00:04:00 -0500

    view

---
+++ view/src/App.jsx
@@ -0,0 +1,15 @@
+
+import React from 'react'
+// import logo from './logo.svg';
+
+function App() {
+  return (
+    <div className="App">
+      hola
+      {/* <img src={logo} className="App-logo" alt="logo" /> */}
+
+    </div>
+  );
+}
+
+export default App;
